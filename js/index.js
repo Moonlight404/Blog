6e03794f32866da0ce698d397a63d63a6c3c8ffd
TARCISIO NUNES <tarcnux@gmail.com>
2020-03-05 21:03:11 -0300

    Fix

--- js/index.js
+++ js/index.js
@@ -9,7 +9,9 @@ const app = new Vue({
             "https://www.urbanarts.com.br/imagens/produtos/156199/0/Ampliada/flor-sakura.jpg",
             "https://http2.mlstatic.com/10-sementes-de-lindas-flores-sakura-flor-de-cerejeira-japo-D_NQ_NP_642015-MLB25125600630_102016-F.jpg",
             "https://2.bp.blogspot.com/-j11PpBudj44/W1NQZAMfqRI/AAAAAAAAIsU/NhJGE9G6CqUAHEbjQjZ2evWgg2o1I8N8wCLcBGAs/s1600/cerejeiras_01.jpg",
-            "https://www.anotaai.blog.br/wp-content/uploads/Flor-de-cerejeira-1.jpg"
+            "https://www.anotaai.blog.br/wp-content/uploads/Flor-de-cerejeira-1.jpg",
+            "https://d384u2mq2suvbq.cloudfront.net/public/spree/products/1597/jumbo/Japanese-Cherry-Blossom-Fragrance-Oil.jpg?1529607178",
+            "https://i.pinimg.com/originals/0e/27/6b/0e276b5fa0eb38dbe3d88337ef79ee6a.jpg"
         ]
     },
     mounted(){
